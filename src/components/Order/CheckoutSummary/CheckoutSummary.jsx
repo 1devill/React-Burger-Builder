d689c21e8dd2d6ebcfd6277d15ce3aa24efebc14
Daniil <dannydeceiver@gmail.com>
2020-02-02 12:43:45 +0200

    started to adding some routing

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Burger from "../../Burger/Burger";
+import Button from "../../UI/Button/Button";
+import classes from "./CheckoutSummary.module.css";
+
+const checkoutSummary = props => {
+  return (
+    <div className={classes.CheckoutSummary}>
+      <h1>Hope it tastes good!</h1>
+      <div style={{ margin: "auto", width: "100%", height: "100%"}}>
+        <Burger ingredients={props.ingredients} />
+        <Button 
+          btnType="Order"
+          btnText="Cancel"
+          clicked={props.checkoutCanceled} />
+        <Button
+          btnType="Success" 
+          btnText="Submit" 
+          clicked={props.checkoutContinued} />
+      </div>
+    </div>
+  );
+};
+
+export default checkoutSummary;
